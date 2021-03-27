2a4b61ae2e7378cf06349aeb50c9d41eb87c053d
Vladyslav Lubeznyy <avinean@Vladyslavs-Mac-mini-2.local>
2021-03-28 01:37:01 +0200

    added user store for sockets

---
+++ server-app/src/SocketsAPI/UsersStore.ts
@@ -0,0 +1,42 @@
+import { User } from "@doer/entities";
+
+export interface UserConfig {
+  socketId: string;
+  user: User;
+}
+
+export interface UsersStoreInterface {
+  users: UserConfig[];
+  add(config: UserConfig): void;
+  remove(userID: number): void;
+  getBySocketID(socketId: string): UserConfig;
+  getByUserID(userID: number): UserConfig;
+}
+
+export default {
+  users: [],
+  add(config: UserConfig) {
+    const existConfig = this.getBySocketID(config.socketId) || this.getByUserID(config.user.id);
+    if (existConfig) {
+      Object.assign(existConfig, config)
+    } else {
+      this.users.push(config);
+    }
+  },
+  remove(userID: number) {
+    const index = this.getByUserID(userID);
+    if(~index) {
+      this.users.splice(index, 1);
+    }
+  },
+  getBySocketID(socketId: string) {
+    return this.users.find((config: UserConfig) => {
+      return config.socketId === socketId;
+    })
+  },
+  getByUserID(userID: number) {
+    return this.users.find((config: UserConfig) => {
+      return config.user.id === userID;
+    })
+  }
+} as UsersStoreInterface;
